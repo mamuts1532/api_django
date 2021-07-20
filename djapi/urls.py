4fc5b719ffe6bf27f5bfefc5c4fe2f91c7af2357
Jorge Guevara <jorge841124@gmail.com>
2021-07-20 06:53:09 -0500

    first commit

---
+++ djapi/urls.py
@@ -0,0 +1,19 @@
+from  django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import(
+TokenObtainPairView,
+TokenRefreshView,
+)
+
+from .views import prueba, DocumentoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'docs', DocumentoViewSet)
+
+urlpatterns = [
+    #path('', prueba, name="prueba"),
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
---
+++ djapi/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework import viewsets
+from .models import Documento
+from .serializer import DocumentoSerializer
+
+
+def prueba(request):
+    return HttpResponse('Primer Vista del Curso!')
+
+class DocumentoViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Documento.objects.all().order_by('id')
+    serializer_class = DocumentoSerializer
